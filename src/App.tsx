4723441b06c0bcfbbf710d36b35d0b422f98d254
bokazott <64536239+bokazott@users.noreply.github.com>
2023-10-02 18:57:32 +0200

    Register form with React+Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {z, ZodType} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from '@hookform/resolvers/zod';
 
+type FormData={
+    firstName:string;
+    lastName:string;
+    email:string;
+    password:string;
+    confirmPassword:string;
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const schema:ZodType<FormData> =z.object({
+        firstName: z.string().min(2).max(30),
+        lastName: z.string().min(2).max(30),
+        email: z.string().email(),
+        password:z.string().min(5).max(20),
+        confirmPassword: z.string().min(5).max(20),
+    }).refine((data)=>data.password===data.confirmPassword,{
+        message:"Passwords do not match",
+        path:["confirmPassword"],
+    });
+
+    const {register,
+        handleSubmit,
+        formState:{errors,}
+        }=useForm<FormData>({resolver:zodResolver(schema)});
+
+    const submitData=(data:FormData)=>{
+        console.log("It worked",data)
+    }
+    return (
+    <div className='container-fluid'>
+        <div className='row mt-5'>
+            <div className='col'></div>
+            <div className='col-md-4 p-md-4'>
+            <form onSubmit={handleSubmit(submitData)}>
+                <h3>Registration</h3>
+                <div>
+                    <label htmlFor='fname'></label>
+                    <input type='text'{...register("firstName")} placeholder='Enter your name' className='form-control'/>
+                    {errors.firstName && <span>{errors.firstName.message}</span>}
+                </div>
+                <div>
+                    <label htmlFor='lname'></label>
+                    <input type='text' {...register("lastName")} placeholder='Enter your last name' className='form-control'/>
+                    {errors.lastName && <span>{errors.lastName.message}</span>}
+                </div>
+                <div>
+                    <label htmlFor='email'></label>
+                    <input type='email' {...register("email")} placeholder='Enter your email' className='form-control'/>
+                    {errors.email && <span>{errors.email.message}</span>}
+                </div>
+                <div>
+                    <label htmlFor='password'></label>
+                    <input type='password' {...register("password")} placeholder='Enter your password' className='form-control'/>
+                    {errors.password && <span>{errors.password.message}</span>}
+                </div>
+                <div>
+                    <label htmlFor='password'></label>
+                    <input type='password' {...register("confirmPassword")} placeholder='Confrim your password' className='form-control'/>
+                    {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
+                </div>
+                <div>
+                    <button type='submit' className='btn btn-primary mt-2'>Register now</button>
+                </div>
+            </form>
+            </div>
+            <div className='col'></div>
+        </div>
     </div>
   );
 }
